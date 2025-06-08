6660b2ac12b61062081f460046808756207c2e4f
thomas.bauer <thomas.bauer@qsc.com>
2025-06-08 21:29:03 +0200

    updated the init

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 ip=192.168.177.51
 
 ssh root@$ip "mkdir -p wallcontroller"
-scp led.py root@$ip:wallcontroller/led.py
-scp main.py root@$ip:wallcontroller/main.py
-scp button.py root@$ip:wallcontroller/button.py
-scp cert.pem root@$ip:wallcontroller/cert.pem
-scp key.pem root@$ip:wallcontroller/key.pem
+scp led.py root@$ip:/usr/bin/gpio-daemon/led.py
+scp main.py root@$ip:/usr/bin/gpio-daemon/main.py
+scp button.py root@$ip:/usr/bin/gpio-daemon/button.py
+scp cert.pem root@$ip:/usr/bin/gpio-daemon/cert.pem
+scp key.pem root@$ip:/usr/bin/gpio-daemon/key.pem
